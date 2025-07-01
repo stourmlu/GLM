9ba768e6fecd9a8a8ed4bbe58d3694ae2a5952c8
stourmlu <ludovic.stourm@gmail.com>
2025-07-01 18:15:13 +0200

    Adding a function to estimate MNL model

---
+++ Matlab/MultinomialLogit/estimate_MNL.m
@@ -0,0 +1,31 @@
+function [beta_star, LL_star, LL_grad, FisherInfo, beta_ses] = estimate_MNL(M, X, Y, nn_vec)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% This function estimates a multinomial logit model by maximum likelihood, using the Newton-Raphson algorithm.
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%%%%% Inputs:
+	% M:					N x 1        (number of tries for choice set with index nn)
+	% X:					dim1 x NumX  (covariates corresponding to each option, across all choice sets)
+	% Y:					dim1 x 1     (number of "successful tries" for that option in that choice set, across the M_n tries)
+	% nn_vec:				dim1 x 1     (maps each option to the choice set it belongs to, values between 1 and N)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%%%%% Outputs:
+	% beta_star:           	NumX x 1
+	% LL_star:				scalar
+	% grad:					NumX x 1
+	% FisherInfo:			NumX x NumX
+	% beta_ses:				NumX x 1
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	
+	M_rep = M(nn_vec);
+
+	LL_constant = sum(gammaln(M+1)) - sum(gammaln(Y+1));
+
+	obj = @(beta_val) loglikelihood_MNL(Y, X, M, M_rep, nn_vec, beta_val, LL_constant, true);
+	beta0 = zeros(size(X,2), 1);
+	tic
+	[beta_star, LL_star, LL_grad, FisherInfo] = Newton_Raphson(obj, beta0, 1e-6, 2000);
+	toc
+	
+	% Compute std. errors
+	beta_ses = sqrt(diag(inv(FisherInfo)));
+end
--- Matlab/MultinomialLogit/z_demo_MNL.m
+++ Matlab/MultinomialLogit/z_demo_MNL.m
@@ -1,3 +1,5 @@
+addpath('../../../Optimization/Matlab');
+
 L = 15;
 N = 4000;
 J = 80;
@@ -10,7 +12,6 @@ M = 100*ones(N, 1);
 nn_vec = repmat([1:N], J, 1); % dim1 x 1 (integers between 1 and N)
 nn_vec = reshape(nn_vec, [dim1 1]); % This is full cartesian product
 
-M_rep = M(nn_vec);
 beta_true = normrnd(0,1,[L 1]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -33,17 +34,6 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Estimate model
-
-LL_constant = sum(gammaln(M+1)) - sum(gammaln(Y+1));
-
-addpath('../../../Optimization/Matlab');
-obj = @(beta_val) loglikelihood_MNL(Y, X, M, M_rep, nn_vec, beta_val, LL_constant, true);
-beta0 = zeros(L, 1);
-tic
-[beta_star, LL_star, LL_grad, FisherInfo] = Newton_Raphson(obj, beta0, 1e-6, 2000);
-toc
-
-% Compute std. errors
-beta_ses = sqrt(diag(inv(FisherInfo)));
+[beta_star, LL_star, LL_grad, FisherInfo, beta_ses] = estimate_MNL(M, X, Y, nn_vec);
 
 disp(table(beta_true, beta_star, beta_ses));
