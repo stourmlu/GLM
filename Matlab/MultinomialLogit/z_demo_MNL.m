2dd3a9cbf596f1ee36ee4b111546773c08e4e139
stourmlu <ludovic.stourm@gmail.com>
2026-01-09 16:17:41 +0100

    Update z_demo_MNL.m

--- Matlab/MultinomialLogit/z_demo_MNL.m
+++ Matlab/MultinomialLogit/z_demo_MNL.m
@@ -3,27 +3,26 @@ addpath('../../../Optimization/Matlab');
 L = 15;
 N = 4000;
 J = 80;
-dim1 = N*J;
 
-X = normrnd(0,1,[dim1 L]);
+X = normrnd(0,1,[J*N L]);
 
 M = 100*ones(N, 1);
 
-nn_vec = repmat([1:N], J, 1); % dim1 x 1 (integers between 1 and N)
-nn_vec = reshape(nn_vec, [dim1 1]); % This is full cartesian product
+nn_vec = repmat([1:N], J, 1); % This is full cartesian product
+nn_vec = reshape(nn_vec, [J*N 1]); % (J*N) x 1 (integers between 1 and N)
 
 beta_true = normrnd(0,1,[L 1]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Generate Y
-V = X * beta_true; % dim1 x 1
+V = X * beta_true; % (J*N) x 1
 Vmax = accumarray(nn_vec, V, [N 1], @max); % N x 1
 V = V - Vmax(nn_vec); % dim1 x 1
 tmp = log(accumarray(nn_vec, exp(V), [N 1])); % N x 1
 logp = V - tmp(nn_vec); % dim1 x 1
-p = exp(logp); % dim1 x 1
+p = exp(logp); % (J*N) x 1
 
-Y = zeros(dim1, 1);
+Y = zeros(J*N, 1);
 for nn = 1:N
 	idces_nn = find(nn_vec == nn); % Jn x 1
 	p_nn = p(idces_nn); % Jn x 1
